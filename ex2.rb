d090a9ec7f974c85233dee6fdb5a8a37e61a86aa
Nathaniel Thompson <nthompson55@gmail.com>
2013-09-24 16:46:42 -0500

    first commit

---
+++ ex2.rb
@@ -0,0 +1,9 @@
+#A comment, this is so that you can read your program later
+#anything after the # us ugnored by ruby
+
+puts "I could have code like this" #and the comments after is ignored
+
+#you can also use a comment to "disable or comment out a piece of cose:
+#puts "This wont run"
+puts "this will run."
+
